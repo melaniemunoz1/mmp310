5b933cb2887b0b547275f6e92b7e4b5889a439c2
melaniemunoz1 <melanie.m1120@gmail.com>
2020-01-29 19:07:31 -0500

    characters

--- characters/sketch.js
+++ characters/sketch.js
@@ -1,13 +1,39 @@
 /*
 Characters sketch 1/27/2020
 */
+var cat;
+var dog;
+var house;
+var story="Torn by two pets";
 
+function preload(){
+	cat = loadImage('cat.png');
+	dog = loadImage('dog.png');
+	house=loadImage('house.png');
+}
+
+var catX=0;
+var dogX=800;
+var houseX=500;
+
+var catY=300;
+var dogY=300;
+var houseY=300;
 
 function setup(){
-	createCanvas (windowWidth. windowHeight);
+	createCanvas (windowWidth, windowHeight);
 
 }
 
 function draw(){
-	backgrou
+	background('pink');
+
+	image(cat,catX,catY);
+	image(dog,dogX,dogY);
+	image(house,houseX,houseY);
+
+	textSize(40);
+	textAlign(CENTER,CENTER);
+	text(story,width/4,20,width/2);
+
 }
