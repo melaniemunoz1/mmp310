d47a7e1f5c43ad557ce804ff7c07d3b9e1e8bba2
melaniemunoz1 <melanie.m1120@gmail.com>
2020-02-26 19:16:15 -0500

    loopnrandom

--- loop/sketch.js
+++ loop/sketch.js
@@ -1,1 +1,26 @@
-var fishImage, cloudImage, treeImage;
+var snowflakeImage;
+
+function preload(){
+	snowflakeImage=loadImage('snowflake.png')
+}
+function setup (){
+	createCanvas(windowWidth,windowHeight);
+	setting();
+}
+
+function mousePressed(){
+	setting();
+}
+function setting(){
+	background('lightblue');
+
+for (let x=55; x <= width; x+=500){
+	image(snowflakeImage,x,5-x/10);
+	
+}
+for (let x=100;x <= width; x+=200){
+	let y = height;
+	let offset=random(-1000,0);
+	image(snowflakeImage,x, y+offset);
+}
+}
---
+++ random/sketch.js
@@ -0,0 +1,1 @@
+var fishImage, cloudImage, treeImage;
